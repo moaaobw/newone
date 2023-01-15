e1685a17da67544cc6e857facee994c70710c750
mohamed beper <106493217+moaaobw@users.noreply.github.com>
2023-01-15 07:57:23 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -40,7 +40,7 @@ SONG_DOWNLOAD_DURATION = int(
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001667306793"))
 
 # A name for your Music bot.
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME"mohalwb", "mr mode")
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME"mohalwb")
 
 # Your User ID.
 OWNER_ID = list(
